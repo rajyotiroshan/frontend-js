687bbad9baa115d29a298c333362a8d2856bac41
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-02-10 10:36:37 +0530

    chart

---
+++ music-player-html-css-major-proj/music-player/script.js
@@ -0,0 +1,126 @@
+let songs = [
+  {
+    id: 1,
+    name: "moh moh ke",
+    artist: "arijit singh",
+    img: "",
+    genre: "romantic",
+    source: "",
+  },
+  {
+    id: 2,
+    name: "moh moh ke",
+    artist: "arijit singh",
+    img: "",
+    genre: "romantic",
+    source: "",
+  },
+  {
+    id: 3,
+    name: "moh moh ke",
+    artist: "arijit singh",
+    img: "",
+    genre: "romantic",
+    source: "",
+  },
+  {
+    id: 4,
+    name: "moh moh ke",
+    artist: "khan",
+    img: "",
+    genre: "qawwali",
+    source: "",
+  },
+  {
+    id: 5,
+    name: "moh moh ke",
+    artist: "khan",
+    img: "",
+    genre: "qawwali",
+    source: "",
+  },
+  {
+    id: 6,
+    name: "moh moh ke",
+    artist: "khan",
+    img: "",
+    genre: "qawwali",
+    source: "",
+  },
+  {
+    id: 7,
+    name: "moh moh ke",
+    artist: "khan",
+    img: "",
+    genre: "pop",
+    source: "",
+  },
+  {
+    id: 8,
+    name: "moh moh ke",
+    artist: "jackson",
+    img: "",
+    genre: "pop",
+    source: "",
+  },
+  {
+    id: 9,
+    name: "moh moh ke",
+    artist: "jackson",
+    img: "",
+    genre: "pop",
+    source: "",
+  },
+  {
+    id: 10,
+    name: "moh moh ke",
+    artist: "jackson",
+    img: "",
+    genre: "ghazal",
+    source: "",
+  },
+  {
+    id: 11,
+    name: "moh moh ke",
+    artist: "sonu nigam",
+    img: "",
+    genre: "ghazal",
+    source: "",
+  },
+  {
+    id: 12,
+    name: "moh moh ke",
+    artist: "sonu nigam",
+    img: "",
+    genre: "ghazal",
+    source: "",
+  },
+  {
+    id: 13,
+    name: "moh moh ke",
+    artist: "sonu nigam",
+    img: "",
+    genre: "ghazal",
+    source: "",
+  },
+];
+
+function toggleTheme() {
+  //dark or light
+}
+
+function showSongs() {
+  //
+}
+
+function renderCurrentSong() {}
+
+//handller for add to playlist
+function addToPlaylist() {}
+
+//click handler for createPlaylist
+function createPlaylist() {}
+
+//click handler for all playlist
+//display songs in thatplaylist
+function renderPlaylistSong() {}
