d6ad8a9093fa57b07d7e403ec02afb4a7c28ebd5
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-02-15 15:12:47 +0530

    mp:: added comment to state func

--- music-player/js/modules/state/state.js
+++ music-player/js/modules/state/state.js
@@ -1,7 +1,13 @@
 import { songs } from "../../songsData.js";
 import { updatePlayerUI } from "../player/player.js";
 let currSong = songs[0];
-
+let playlist = {}; //{name: [songid1, songid2,..., songid3]}
+let currPlaylist = {}
+/**
+ *
+ * @param {songID} songID
+ * @returns "updae currSong with passed songID->song"
+ */
 function updateCurrSong(songID) {
   let songToPlay = songs.find(({ id }) => id === songID);
   if (!currSong) {
@@ -11,6 +17,12 @@ function updateCurrSong(songID) {
   currSong = songToPlay;
   updatePlayerUI(currSong);
 }
+
+/**
+ *
+ * @param {currSongID} currently playing song's id
+ * @returns "get next song id from songs list"
+ */
 function getNextSongID(currSongID) {
   let currSongIndex = songs.findIndex(({ id }) => id === currSongID);
   let nextSongIndex = currSongIndex + 1;
@@ -20,6 +32,11 @@ function getNextSongID(currSongID) {
   return songs[nextSongIndex].id;
 }
 
+/**
+ *
+ * @param {currSongID} currently playing song's id
+ * @returns "get prev song id from songs list"
+ */
 function getPrevSongID(currSongID) {
   let currSongIndex = songs.findIndex(({ id }) => id === currSongID);
   let prevSongIndex = currSongIndex - 1;
@@ -28,4 +45,5 @@ function getPrevSongID(currSongID) {
   }
   return songs[prevSongIndex].id;
 }
+
 export { currSong, updateCurrSong, getNextSongID, getPrevSongID };
