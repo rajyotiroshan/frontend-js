4e874ed37bd8fe214053747da670b8b33daa6c25
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-04-21 17:18:26 +0530

    seperate js file for function related to state change

---
+++ mealApp/state/stateFunctions.js
@@ -0,0 +1,69 @@
+import { renderMealsList } from "../js/module/mealsList.js";
+import mealsState from "./state.js";
+/**
+ * replace mealsList with meals
+ * @param {fetched meals list} meals
+ */
+function updateMealsList(meals) {
+  mealsState.fetchedMeals = meals;
+  let mealObj = {
+    ingredients: {},
+    measures: {},
+  };
+
+  let mealsList = {};
+
+  //extract ingredients and measures
+  /// console.log(meals);
+  meals.forEach((mObj) => {
+    mealObj.ingredients[mObj["idMeal"]] = [];
+    mealObj.measures[mObj["idMeal"]] = [];
+    Object.keys(mObj).forEach((key) => {
+      if (
+        key.startsWith("strIngredient") &&
+        mObj[key] !== null &&
+        mObj[key].trim() !== ""
+      ) {
+        mealObj.ingredients[mObj["idMeal"]].push(mObj[key]);
+      }
+
+      if (
+        key.startsWith("strMeasure") &&
+        mObj[key] !== null &&
+        mObj[key].trim() !== ""
+      ) {
+        mealObj.measures[mObj["idMeal"]].push(mObj[key]);
+      }
+    });
+
+    mealsList[mObj["idMeal"]] = mObj;
+  });
+
+  //console.log(mealObj);
+
+  mealsState.meals.mealsList = mealsList;
+  mealsState.meals.ingredients = mealObj.ingredients;
+  mealsState.meals.measures = mealObj.measures;
+
+  console.log(mealsState);
+
+  renderMealsList(meals);
+}
+
+/**
+ * add pass eal id to favourite meal list
+ * @param {meal id to add to fav list} mealID
+ */
+function addMealToFav(mealID) {
+  mealsState.favList.push(mealID);
+}
+
+/**
+ * remove passed meal id from favourite meal list
+ * @param {meal id} mealID
+ */
+function removeMealFromFav(mealID) {
+  mealsState.favList = mealsState.favList.forEach((id) => id != mealID);
+}
+
+export { mealsState, updateMealsList, addMealToFav, removeMealFromFav };
