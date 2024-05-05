89d6dba9c360d4457326cb165d7aac680b87076d
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-05-05 22:35:08 +0530

    store fav meal id in ocal storage

--- mealApp/js/module/favourite.js
+++ mealApp/js/module/favourite.js
@@ -4,11 +4,30 @@ function addClickEvtToFavBtn() {
 
   favBtnList.forEach((el) => {
     el.addEventListener("click", (evt) => {
-      console.log(evt.target.getAttribute("data-mealid"));
+      debugger;
+      //console.log(evt.target.getAttribute("data-mealid"));
+      const mealID = evt.target.getAttribute("data-mealid").trim();
       //add id to fav
       if (!evt.target.classList.contains("selected")) {
+        let favListID = localStorage.getItem("favListID");
+        if (favListID) {
+          favListID = JSON.parse(favListID);
+          favListID.push(mealID);
+        } else {
+          favListID = [];
+          favListID.push(mealID);
+        }
+        localStorage.setItem("favListID", JSON.stringify(favListID));
         evt.target.classList.add("selected");
       } else {
+        //remove from list.
+        let favListID = localStorage.getItem("favListID");
+        if (favListID) {
+          favListID = JSON.parse(favListID);
+          favListID = favListID.filter((id) => id != mealID);
+          favListID.push(mealID);
+          localStorage.setItem("favListID", JSON.stringify(favListID));
+        }
         evt.target.classList.remove("selected");
       }
     });
